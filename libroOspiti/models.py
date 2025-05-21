34e89aff3b27bcbb3ad40352444c8a6c08c20c23
HOXHAJ SIMONE <SI.HOXHAJ@Magistri.local>
2025-05-21 12:48:24 +0200

    modifica models.py+

--- libroOspiti/models.py
+++ libroOspiti/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Autore(models.Model):
+    nome = models.CharField(max_length=50)
+    cognome = models.CharField(max_length=50)
+
+class Commento(models.model):
+    data = models.DateField(auto_now_add=True)
+    testo = models.TextField()
+    autore = models.ForeignKey(Autore,on_delete=models.CASCADE)
+
+    autoriPreferiti = models.ManyToManyField(Autore,related_name="preferiti")
