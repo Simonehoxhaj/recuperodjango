256c950764205ba1b03f6e9e1c07e0b22ff3001d
Simonehoxhaj <SI.HOXHAJ@Magistri.local>
2025-05-21 13:42:25 +0200

    aggiunta logica per salvataggio commenti

--- libroOspiti/views.py
+++ libroOspiti/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
+from .models import Autore,Commento
 
 def index(request):
+    #Il post aggiunge il messaggio
+    if request.method == 'POST':
+        user = request.POST.get('username')
+        text = request.POST.get('message')
+        #Se ci sono effettivamente i dati desiderati
+        if user and text:
+            #Controllo se l'utente esiste già
+            numero = Autore.objects.filter(username=user).count()
+            if numero == 0: #Se l'utente non esiste, lo creo
+                Autore.objects.create(username=user)
+
+            #Prendo l'autore
+            aut = Autore.objects.filter(username=user).first()
+
+            #Creo il commento
+            Commento.objects.create(testo=text,autore=aut)
+
+            messages.success(request, 'Il tuo messaggio è stato inviato con successo!')
+            print(id)
+        else:
+            messages.error(request, 'Errore nell\'invio del messaggio. Riprova.',extra_tags="danger")
+
+
     return render(request, 'libroOspiti/home.html')
 
 
 def messaggi(request):
-    return render(request,"libroOspiti/messaggi.html")
+    
+    messaggi = Commento.objects.all()
+    return render(request,"libroOspiti/messaggi.html",{"messaggi":messaggi})
